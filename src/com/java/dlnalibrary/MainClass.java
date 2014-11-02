ffe2983d9777c45f0f923640c37104d65e6416bb
Karthik Iyengar <karthikeyan.iyengar@gmail.com>
2014-11-03 02:08:35 +0530

    Makeshift solution for metadata

--- src/com/java/dlnalibrary/MainClass.java
+++ src/com/java/dlnalibrary/MainClass.java
@@ -3,14 +3,10 @@ package com.java.dlnalibrary;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
-import org.teleal.cling.model.meta.Service;
-import org.teleal.cling.model.types.UDAServiceId;
 
 public class MainClass {
 	public static void main(String args[]) {
 	
-		
-		
 		final ServiceProvider serv = new ServiceProvider();
 		
 		
@@ -27,22 +23,37 @@ public class MainClass {
 			public void run() {				
 				//Retrieve HashMap of devices
 				
-				HashMap<String, String> list = serv.getDevicesHashMap();
+				HashMap<String, String> list = serv.getDeviceList();
 				System.out.println(list.toString());
 				
-				//Shutdown Discovery
-				//serv.stopDiscovery();
-				serv.selectDevice("some uuid");
-				serv.subscribeEvents();
-				//serv.getDetails();
+				
+				/*
+				 * BubbleSoft - Karthik - d6ae4960-aacb-6493-0000-0000534ff697
+				 * 				Bala - 21e829cb-230c-5f94-0000-000039b28a64
+				 * ArchLaptop - 7c94075d-88a7-c9ef-fbfd-6eabba1aa291
+				 */
+				
+				//serv.selectDevice("7c94075d-88a7-c9ef-fbfd-6eabba1aa291");
+				serv.selectDevice("21e829cb-230c-5f94-0000-000039b28a64");
+				
+				//serv.subscribeEvents();
+				//serv.addStream();
+				
 				//serv.sendStream("http://192.168.1.55:8000/01%20-%20Mama%20Kin.mp3");
-				//serv.sendStream("http://192.168.1.55:8000/ABC.mp3");
-				//serv.play();
+				
 				//serv.seek("00:00:20");
 				//serv.stop();
 			}
 		}, 4000);
 		
-		
+		timer.schedule(new TimerTask() {
+			public void run() {
+				
+				serv.sendStream("http://192.168.1.55:8000/ABC.mp3", "Crappy Hindi Song", "jnjl","hjk");
+				//serv.sendStream("http://192.168.1.55:8000/Happy.mp4", "DWBH", "jnjl","hjk");
+				serv.play();
+				serv.getDetails();
+			}
+		}, 6000);
 	}
 }
--- src/com/java/dlnalibrary/ServiceProvider.java
+++ src/com/java/dlnalibrary/ServiceProvider.java
@@ -1,7 +1,9 @@
 package com.java.dlnalibrary;
 
-import java.util.Collection;
+import java.net.URI;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.teleal.cling.UpnpService;
 import org.teleal.cling.UpnpServiceImpl;
@@ -12,17 +14,18 @@ import org.teleal.cling.model.gena.CancelReason;
 import org.teleal.cling.model.gena.GENASubscription;
 import org.teleal.cling.model.message.UpnpResponse;
 import org.teleal.cling.model.message.header.STAllHeader;
-import org.teleal.cling.model.message.header.UDADeviceTypeHeader;
+import org.teleal.cling.model.meta.Action;
 import org.teleal.cling.model.meta.Device;
 import org.teleal.cling.model.meta.LocalDevice;
 import org.teleal.cling.model.meta.RemoteDevice;
-import org.teleal.cling.model.meta.RemoteDeviceIdentity;
 import org.teleal.cling.model.meta.Service;
-import org.teleal.cling.model.types.UDADeviceType;
 import org.teleal.cling.model.types.UDAServiceId;
 import org.teleal.cling.model.types.UDN;
+import org.teleal.cling.model.types.UnsignedIntegerFourBytes;
 import org.teleal.cling.registry.Registry;
 import org.teleal.cling.registry.RegistryListener;
+import org.teleal.cling.support.avtransport.callback.GetMediaInfo;
+import org.teleal.cling.support.avtransport.callback.GetTransportInfo;
 import org.teleal.cling.support.avtransport.callback.Pause;
 import org.teleal.cling.support.avtransport.callback.Play;
 import org.teleal.cling.support.avtransport.callback.Seek;
@@ -30,16 +33,20 @@ import org.teleal.cling.support.avtransport.callback.SetAVTransportURI;
 import org.teleal.cling.support.avtransport.callback.Stop;
 import org.teleal.cling.support.avtransport.lastchange.AVTransportLastChangeParser;
 import org.teleal.cling.support.avtransport.lastchange.AVTransportVariable;
+import org.teleal.cling.support.contentdirectory.callback.Browse;
+import org.teleal.cling.support.contentdirectory.callback.Browse.Status;
 import org.teleal.cling.support.lastchange.LastChange;
-import org.teleal.cling.support.model.SeekMode;
+import org.teleal.cling.support.model.BrowseFlag;
+import org.teleal.cling.support.model.DIDLContent;
+import org.teleal.cling.support.model.MediaInfo;
 
 public class ServiceProvider {
 	
 	HashMap<String, String> list = new HashMap<String, String>();
 	UpnpService upnpService;	
-	Registry globalRegistry;
 	Service service;
-	
+	LastChange lastChange;
+	private static final Logger log = Logger.getLogger(ServiceProvider.class.getName() );
 	public void startDiscovery() {
 	
 		
@@ -59,21 +66,17 @@ public class ServiceProvider {
 			
 			@Override
 			public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
-				System.out.println("Found: " + device.getDisplayString());
+				log.log(Level.INFO, "Found: " + device.getDisplayString());
 				list.put(device.getIdentity().getUdn().toString(), device.getDisplayString());
-				/*globalRegistry = registry;
-				Collection<Device> devices = registry.getDevices();
-				System.out.println("Ye achha wala" + devices.toString());*/
 			}
 			@Override
 			public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
-				System.out.println("Removed: " + device.getDisplayString());
+				log.log(Level.INFO, "Removed: " + device.getDisplayString());
 				list.remove(device.getIdentity().getUdn().toString());
 			}
 			@Override
 			public void remoteDeviceDiscoveryFailed(Registry registry, RemoteDevice device,
 					Exception ex) {
-				// TODO Auto-generated method stub
 				
 			}
 			
@@ -99,7 +102,7 @@ public class ServiceProvider {
 		upnpService.getControlPoint().search(new STAllHeader());
 	}
 	
-	public HashMap<String, String> getDevicesHashMap() {
+	public HashMap<String, String> getDeviceList() {
 		return list;
 	}
 	
@@ -109,40 +112,32 @@ public class ServiceProvider {
 	}
 	
 	public void selectDevice(String uid) {
-		//upnpService.getRegistry().getDevice(new UDN, arg1)
-		Device device = upnpService.getControlPoint().getRegistry().getDevice(new UDN("7c94075d-88a7-c9ef-fbfd-6eabba1aa291"), false); //boolean = false if embedded device
-		//
-		//UDN udn = new UDN("7c94075d-88a7-c9ef-fbfd-6eabba1aa291");
-		//Device device = registry.getDevice(udn, true);
-		
-		//Collection<Device> devices = upnpService.getRegistry().getDevices();
-		//System.out.println(devices.toString());
-		
-		/*Device outDevice = null;
-		for (Device device : devices) {
-			outDevice = device.findDevice(new UDN("7c94075d-88a7-c9ef-fbfd-6eabba1aa291"));
+		try {
+			Device device = upnpService.getControlPoint().getRegistry().getDevice(new UDN(uid), false); //boolean = false if embedded device
+			service = device.findService(new UDAServiceId("AVTransport"));
+			subscribeEvents();
 		}
-		
-		*/
-		
-		//Device d = device.findDevice(new UDN("b992bebc-d671-798d-328a-268d984773a3"));
-		
-		service = device.findService(new UDAServiceId("AVTransport"));
-		System.out.println("Service " + service == null);
+		catch (Exception e) {
+			log.log(Level.SEVERE, "Error: Device not found. Bad UUID format.");
+		}
 	}
 
-	public void sendStream(String url) {
-		  ActionCallback setAVTransportURIAction =
-	        		new SetAVTransportURI(service, url, "NO METADATA") {
+	public void sendStream(String url, String title, String artist, String album) {
+		String metadata;
+		metadata = "<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\" xmlns:sec=\"http://www.sec.co.kr/\"><item id=\"\" parentID=\"\" restricted=\"\"><upnp:class>object.item.audioItem.musicTrack</upnp:class>object.item.audioItem.musicTrack<dc:title>" + title + "</dc:title><dc:creator></dc:creator><upnp:artist>" + artist + "</upnp:artist><upnp:albumArtURI></upnp:albumArtURI><dc:date></dc:date><upnp:album>" + album + "</upnp:album><upnp:originalTrackNumber>1</upnp:originalTrackNumber><res protocolInfo=\"\" size=\"\" duration=\"\"></res></item></DIDL-Lite>";
+
+		ActionCallback setAVTransportURIAction =
+					new SetAVTransportURI(service, url, metadata) {
 						@Override
 						public void failure(ActionInvocation arg0,
 								UpnpResponse arg1, String arg2) {
 							// TODO Auto-generated method stub
-							
 						}
 	        		};
+	        		//setAVTransportURIAction.getActionInvocation().setInput("CurrentURI", "WHATEVER!");
 	       upnpService.getControlPoint().execute(setAVTransportURIAction);
 	}
+	
 	
 	public void play() {
 		 ActionCallback playAction =
@@ -179,77 +174,69 @@ public class ServiceProvider {
 		upnpService.getControlPoint().execute(pauseAction);
 	}
 	
+
 	public void subscribeEvents() {
-		System.out.println("In subscribe");
 		SubscriptionCallback callback = new SubscriptionCallback(service, 1000) {
 			
 			@Override
 			protected void failed(GENASubscription arg0, UpnpResponse arg1,
 					Exception arg2, String arg3) {
-				// TODO Auto-generated method stub
-				System.out.println("FAIL HUA!" + arg2.getMessage());
+				log.log(Level.WARNING, "GENA Subscription failed!" + arg2.getMessage());
 			}
 			
 			@Override
 			protected void eventsMissed(GENASubscription arg0, int arg1) {
-				// TODO Auto-generated method stub
-				System.out.println("HUAAA!");
+				log.log(Level.INFO, "Event Missed!");
 			}
 			
-			@SuppressWarnings("fallthrough")
+			
 			@Override
 			protected void eventReceived(GENASubscription sub) {
 			    try {
-					LastChange lastChange = new LastChange(
+					lastChange = new LastChange(
 					          new AVTransportLastChangeParser(),
 					          sub.getCurrentValues().get("LastChange").toString()
 					  );
-					
-					
-					System.out.println(lastChange.getEventedValue(0,AVTransportVariable.TransportState.class).getValue());
-				    System.out.println("Duration: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrackDuration.class).getValue());
-				    System.out.println("CurrentItem: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrack.class).getValue());
-				    System.out.println("Status: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentPlayMode.class).getValue());
-				    System.out.println("Metadata: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrackMetaData.class).getValue());
-			    
 			    } catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			    
-			    
 			}
 			
 			@Override
 			protected void established(GENASubscription arg0) {
-				// TODO Auto-generated method stub
-
+				log.log(Level.INFO, "GENA Subscription Established");
 			}
 			
 			@Override
 			protected void ended(GENASubscription arg0, CancelReason arg1,
 					UpnpResponse arg2) {
-				// TODO Auto-generated method stub
-				
+				log.log(Level.INFO, "GENA Subscription Ended");
 			}
 
 		};
 		upnpService.getControlPoint().execute(callback);
 	}
 	
+	 
+	/* getDetails() will cause a NullPointerException exception if called immediately after subscribeEvents() */
+	/* Can use GetMediaInfo()? */
 	public void getDetails() {
-		/*System.out.println(lastChange.getEventedValue(0,AVTransportVariable.TransportState.class).getValue());
+		try {
         System.out.println("Duration: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrackDuration.class).getValue());
-        System.out.println("CurrentItem: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrack.class).getValue());
-        System.out.println("Status: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentPlayMode.class).getValue());
-        System.out.println("Metadata: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrackMetaData.class).getValue());*/
+        System.out.println("CurrentItem: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrackURI.class).getValue());
+        System.out.println("Status: "+lastChange.getEventedValue(0, AVTransportVariable.TransportState.class).getValue());
+        System.out.println("Metadata: "+lastChange.getEventedValue(0, AVTransportVariable.CurrentTrackMetaData.class).getValue());
+        
+		}
+		catch (NullPointerException e) {
+			System.out.println("None");
+		}
 	}
 	public void seek(String time) {
 		ActionCallback seekAction = new Seek(service, time) {
 			@Override
 			public void failure(ActionInvocation arg0, UpnpResponse arg1, String arg2) {
-				// TODO Auto-generated method stub
-				System.out.println("FAILED!");
+				log.log(Level.WARNING, "Seek Failed!");
 			}
 		};
 		upnpService.getControlPoint().execute(seekAction);
